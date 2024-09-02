ff7ee7b8d36e22984589371b9b6874b05bd3389d
valemr05 <valeria.munoz3@utp.edu.co>
2024-09-02 16:48:08 -0500

    user final

--- ui/user.py
+++ ui/user.py
@@ -3,13 +3,16 @@ def user_input():
     register = int(input("Ingrese el número de registros que desea consultar: "))
 
     return department, register
+
 def display_data(filtered_df):
-    for index, row in filtered_df.iterrows():
-        print("Ciudad: {0}, Departamento: {1}, Edad: {2}, Tipo de contagio: {3}, Estado: {4}, Recuperado: {5}".format(
-            row['ciudad_municipio_nom'],
-            row['departamento_nom'],
-            row['edad'],
-            row['fuente_tipo_contagio'],
-            row['estado'],
-            row['recuperado']
-        ))
+    renamed_df = filtered_df.rename(columns={
+        "ciudad_municipio_nom": " Ciudad ",
+        "departamento_nom": " Departamento ",
+        "edad" : "edad",
+        "fuente_tipo_contagio": "Tipo de Contagio",
+        "estado": "Estado",
+        "recuperado": "Recuperado"
+    })
+    print(renamed_df)
+
+
